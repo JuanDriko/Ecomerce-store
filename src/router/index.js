c803ad90525d0da0768ad7eec150890173b2b1a9
JuanDriko <juandriko@hotmail.com>
2024-03-22 12:42:56 -0500

    Store2

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
+      path: '/Ecomerce-store/',
       name: 'Dashboard',
       component: Dashboard
     },
