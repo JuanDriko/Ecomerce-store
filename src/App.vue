3b8d13d88f75b03084be317f03058e19fcfc5e2c
JuanDriko <juandriko@hotmail.com>
2024-03-22 12:26:51 -0500

    Store

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Navbar/>
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+/* Estilos */
+</style>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
